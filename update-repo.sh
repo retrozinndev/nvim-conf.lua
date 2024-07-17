473efc1c719db429fe69de0dab5a13953372865e
João Dias <joaovodias@gmail.com>
2024-07-17 12:12:32 -0300

    add script to update repo with current config

---
+++ update-repo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "[warn] this script is used to update the repo with current user configuration."
+echo "[tip] press Ctrl + C to quit the update script."
+echo "waiting 3 seconds to start..."
+sleep 3
+
+printf "\n"
+
+echo "[info] deleting current repo's configuration"
+rm -rf init.lua lua/
+
+echo "[info] copying user nvim configuration to current dir"
+cp -r $HOME/.config/nvim/* .
+
+echo "[info] removing unnecessary files"
+rm -rf lazy-lock.json pack/
+
+printf "\n" && git status && printf "\n"
+
+echo "Done! Quitting script."
+exit 0;
