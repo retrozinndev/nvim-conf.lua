4999b566d0e53685048c8bac3b4e3c3bc7c703ad
João Dias <joavodias@gmail.com>
2024-09-29 10:39:30 -0300

    :sparkles: feat(plugins): add emmet, startup, tailwind, telescope and window picker

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -27,7 +27,7 @@ return {
 
       if vim.bo.filetype == "netrw" and vim.b.netrw_method == nil then
           vim.defer_fn(function() 
-              vim.cmd("enew | Neotree current dir=" .. vim.b.netrw_curdir)
+              vim.cmd("tabnew | Neotree current dir=" .. vim.b.netrw_curdir)
           end, 0)
       end
   end
